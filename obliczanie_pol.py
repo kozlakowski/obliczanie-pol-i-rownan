e9a688edfa20a220da49a191a9e0e2daf4eddcc2
Michal Olejnik <kozlakowski48@gmail.com>
2017-10-21 21:09:00 +0200

    Dodane obliczanie pola kola

--- obliczanie_pol.py
+++ obliczanie_pol.py
@@ -1,3 +1,6 @@
+import math
+
+
 def get_area(shape): # Nastepnie zajmuje sie tym
 
     shape = shape.lower()
@@ -7,6 +10,10 @@ def get_area(shape): # Nastepnie zajmuje sie tym
 
     elif shape == "kwadrat":
         kwadrat_area()
+
+    elif shape == "kolo":
+        kolo_area()
+
 
     else:
         print("You must pick kwadrat or prostokąt")
@@ -30,6 +37,18 @@ def kwadrat_area():
     area = dlugosc_boku **2
 
     print("Area of the square is :", area)
+
+def kolo_area():
+    promien = float(input("Ener the radius : "))
+
+    area = math.pi * (promien **2)
+
+    print("Area of the circle is :", area)
+
+
+
+
+
 
 
 
